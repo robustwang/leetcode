9835e5dabc7b4f4cf6c0043e64d6ed6b22095677
wangjun <wang.jun@dr-elephant.com>
2019-11-27 15:34:06 +0800

    #增加 树交换 合并 最大深度

---
+++ src/main/java/com/wangjun/leetcode/tree/InvertTree.java
@@ -0,0 +1,47 @@
+package com.wangjun.leetcode.tree;
+
+/**
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ */
+public class InvertTree {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
